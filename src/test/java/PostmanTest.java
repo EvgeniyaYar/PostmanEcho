d4f6d574e96f48c6ee8824e28385e8853f400811
Evgeniya Yarotskaya <evgeniya047@mail.ru>
2023-05-06 01:31:46 -0700

    First check

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,3 +1,19 @@
-public class PostmanTest {
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    public void shouldCheckSomething() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("something"))
+        ;
+    }
 }
